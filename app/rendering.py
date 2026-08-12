69245de3d8b9859a87fb7e2065a7865dac1a055a
DonBellozi <ivan.v.belikov@gmail.com>
2026-08-12 23:52:25 +0300

    Первый релиз

--- app/rendering.py
+++ app/rendering.py
@@ -78,8 +78,8 @@ def email_html(
     wish_block = ""
     if wish_text:
         wish_block = (
-            f'<tr><td style="padding:0 38px 28px 38px;font-family:Georgia,&quot;Times New Roman&quot;,serif;'
-            f'font-size:24px;font-style:italic;font-weight:700;line-height:1.6;'
+            f'<tr><td style="padding:0 38px 28px 38px;font-family:&quot;Times New Roman&quot;,Times,serif;'
+            f'font-size:20pt;font-style:italic;font-weight:700;line-height:1.6;'
             f'color:{text_color};">{html.escape(wish_text)}</td></tr>'
         )
 
@@ -100,8 +100,8 @@ def email_html(
 <table width="100%" cellspacing="0" cellpadding="0" border="0" style="background:{bg};">
 <tr><td align="center" style="padding:24px 12px;">
 <table width="960" cellspacing="0" cellpadding="0" border="0" style="width:100%;max-width:960px;background:#ffffff;">
-<tr><td style="padding:32px 38px 16px 38px;font-family:Georgia,&quot;Times New Roman&quot;,serif;font-size:24px;font-style:italic;font-weight:700;line-height:1.55;color:{text_color};">Добрый день!</td></tr>
-<tr><td style="padding:6px 38px 24px 38px;font-family:Georgia,&quot;Times New Roman&quot;,serif;font-size:27px;font-style:italic;font-weight:700;line-height:1.55;color:{text_color};">{html.escape(intro_text)}</td></tr>
+<tr><td style="padding:32px 38px 16px 38px;font-family:&quot;Times New Roman&quot;,Times,serif;font-size:20pt;font-style:italic;font-weight:700;line-height:1.55;color:{text_color};">Добрый день!</td></tr>
+<tr><td style="padding:6px 38px 24px 38px;font-family:&quot;Times New Roman&quot;,Times,serif;font-size:20pt;font-style:italic;font-weight:700;line-height:1.55;color:{text_color};">{html.escape(intro_text)}</td></tr>
 {position_block}
 {wish_block}
 {card_block}
